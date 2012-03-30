0f7519f6ac86d859c5e69f7ac7716e32704f55cf
Matt Malin <email@mattmalin.co.uk>
2012-03-30 20:28:14 +0100

    DESCRIPTION added, package roxygen info added

---
+++ R/Rface.r
@@ -0,0 +1,12 @@
+#' Facial recognition info from face.com
+#'
+#' Facial recognition using the face.com API
+#' @docType package
+#' @name Rface
+#' @import XML
+#' @author Matt Malin <\email{email@@mattmalin.co.uk}>
+#' @aliases Rface facecom face.com
+NULL
+
+# example api call:
+# http://api.face.com/faces/detect.json?api_key=083db39e47bd98cb8f6a8737bdb97fc2&api_secret=170298e8e133f775551f2c1de627591f&urls=http://upload.wikimedia.org/wikipedia/en/c/c8/Alan_Turing_photo.jpg&detector=Aggressive&attributes=all&
